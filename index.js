9fe5130721603252cf819fdb5034b8cb781768e7
rober1992 <82915340+rober1992@users.noreply.github.com>
2022-06-08 19:30:33 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
-/* const http = require ('http')
-const puerto = 8080
-
-const server = http.createServer((req, res) =>{
-
-}) 
-
-server.listen(puerto, () =>{
-
-}) */
-
 const express = require('express')
+const { json } = require('express/lib/response')
+const fs = require('fs')
 const app = express()
-const puerto = 8080
+const PORT = 8080
+const productos = fs.readFileSync('./productos.txt', "utf-8")
+
+function elementoAleatorio(datos) {
+    if (!Array.isArray(datos)) {
+        throw TypeError('El argumento debe ser un arreglo.');
+    }
+
+    if (!datos.length) {
+        return null;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * datos.length);
+
+    return datos[indiceAleatorio];
+}
+
+try {
+    console.log(elementoAleatorio(JSON.parse(productos)));
+} catch (e) {
+    console.log(`Error: ${e.message}`);
+}
 
 app.get('/productos', (req, res) =>{
-    res.send('Mira nuestros productos')
+    res.send(JSON.parse(productos))
+    
 })
 
-app.listen(puerto, ()=>{
-    console.log(`El servidor se inicio en el puerto ${puerto}`)
+app.get('/productoRandom', (req, res) =>{
+    res.send(elementoAleatorio(JSON.parse(productos)))
+    
 })
+
+app.listen(PORT, ()=>{
+    console.log(`El servidor se inicio en el puerto ${PORT}`)
+    
+})
+
+
+
